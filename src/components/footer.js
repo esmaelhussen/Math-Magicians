d7c6ef8b657e7cf3099166c4fb4202e5a95b0351
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-18 21:30:23 +0300

    Add footer and home layout

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <div className="flex justify-center text-xl font-bold ">&#169;2025 Esmael Hussen, All rights reserved.</div>
+  );
+}
+
+export default Footer;
---
+++ src/components/home.js
@@ -0,0 +1,31 @@
+function Home() {
+  return (
+    <section className="px-72">
+      <h2 className="font-bold text-4xl pb-8">Welcome to our page!</h2>
+      <p className="text-[1.0rem] font-bold pt-8">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+      <p className="text-[1.0rem] font-bold pt-8">
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industrys standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book. It has survived not only five
+        centuries, but also the leap into electronic typesetting, remaining
+        essentially unchanged. It was popularised in the 1960s with the release
+        of Letraset sheets containing Lorem Ipsum passages, and more recently
+        with desktop publishing software like Aldus PageMaker including versions
+        of Lorem Ipsum.
+      </p>
+    </section>
+  );
+}
+
+export default Home;
