8eea4c0bd79079b50c248d3768ab2e301e8f465d
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-18 21:31:35 +0300

    add quote page

---
+++ src/components/quote.js
@@ -0,0 +1,11 @@
+function Quote() {
+  return (
+    <p className="font-bold text-2xl">
+      Mathematics is not about numbers, equations, computations,
+      <br />
+      or algorithms: It is about understanding. -- William Paul Thurston
+    </p>
+  );
+}
+
+export default Quote;
