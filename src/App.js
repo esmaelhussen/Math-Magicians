8dbba17da52d9f7e9af22cbe2a1cca90328e6bf4
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-18 21:28:46 +0300

    Add Home,calculator and quote using route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-// import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header';
 import Calculator from './components/Calculator';
-// import './App.css';
+import Home from './components/home';
+import Quote from './components/quote';
+import Footer from './components/footer';
 
 function App() {
   return (
-    <Calculator />
+    <>
+      <div className="items-center flex justify-center min-h-[90vh]">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/calculator" element={<Calculator />} />
+          <Route path="/quote" element={<Quote />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
   );
 }
 
