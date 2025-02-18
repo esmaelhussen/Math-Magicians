f307baa85b2eaa774ff9ad4c2aec2e49d183843c
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-18 10:00:20 +0300

    remove unwanted div element

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -12,7 +12,7 @@ function Calculator() {
   const handleResult = (event) => {
     const newTotal = event.target.textContent;
     setCalcState((prev) => ({
-      prev,
+      ...prev,
       total: newTotal,
     }));
   };
@@ -25,95 +25,95 @@ function Calculator() {
   const { total, next, operation } = calcState;
 
   return (
-    <section className="section">
-      <div className="button grid grid-cols-[5rem_5rem_5rem_5rem]">
-        <div className="result four-cell col-span-4 bg-[#858694] text-right text-2xl p-2 h-14 border border-solid border-[#cecdcd]" onChange={handleResult}>
-          {total}
-          {operation}
-          {next}
-        </div>
-        <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          AC
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          +/-
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          %
-        </button>
-        <button
-          type="button"
-          className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
-          onClick={handlePressButton}
-        >
-          ÷
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          7
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          8
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          9
-        </button>
-        <button
-          type="button"
-          className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
-          onClick={handlePressButton}
-        >
-          x
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          4
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          5
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          6
-        </button>
-        <button
-          type="button"
-          className="key operator bg-[#ff9832] "
-          onClick={handlePressButton}
-        >
-          -
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          1
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          2
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          3
-        </button>
-        <button
-          type="button"
-          className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
-          onClick={handlePressButton}
-        >
-          +
-        </button>
-        <button
-          type="button"
-          className="key two-cell bg-[#e6e4e4] col-span-2 h-14 border border-solid border-[#cecdcd]"
-          onClick={handlePressButton}
-        >
-          0
-        </button>
-        <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
-          .
-        </button>
-        <button
-          type="button"
-          className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
-          onClick={handlePressButton}
-        >
-          =
-        </button>
+    <section className="section button grid grid-cols-[5rem_5rem_5rem_5rem]">
+
+      <div className="result four-cell col-span-4 bg-[#858694] text-right text-xl p-2 h-14 border border-solid border-[#cecdcd]" onChange={handleResult}>
+        {total}
+        {operation}
+        {next}
       </div>
+
+      <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        AC
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        +/-
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        %
+      </button>
+      <button
+        type="button"
+        className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
+        onClick={handlePressButton}
+      >
+        ÷
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        7
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        8
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        9
+      </button>
+      <button
+        type="button"
+        className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
+        onClick={handlePressButton}
+      >
+        x
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        4
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        5
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        6
+      </button>
+      <button
+        type="button"
+        className="key operator bg-[#ff9832] "
+        onClick={handlePressButton}
+      >
+        -
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] h-14 border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        1
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        2
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        3
+      </button>
+      <button
+        type="button"
+        className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
+        onClick={handlePressButton}
+      >
+        +
+      </button>
+      <button
+        type="button"
+        className="key two-cell bg-[#e6e4e4] col-span-2 h-14 border border-solid border-[#cecdcd]"
+        onClick={handlePressButton}
+      >
+        0
+      </button>
+      <button type="button" className="key bg-[#e6e4e4] border border-solid border-[#cecdcd]" onClick={handlePressButton}>
+        .
+      </button>
+      <button
+        type="button"
+        className="key operator bg-[#ff9832] border border-solid border-[#cecdcd]"
+        onClick={handlePressButton}
+      >
+        =
+      </button>
 
     </section>
   );
