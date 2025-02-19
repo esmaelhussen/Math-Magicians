110e1948d979542a1ec1c62067f625aac2bbdb66
esmael hussen abdu <esmaelhussenabdu@gmail.com>
2025-02-20 02:01:58 +0300

    fix eslint error

--- src/components/calculator.test.js
+++ src/components/calculator.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Calculator from './calculator';
+import Calculator from './calculator.js';
 
 describe('calculator page', () => {
   it('calculator page should be render', () => {
